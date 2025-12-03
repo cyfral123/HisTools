f91926fc1b593a2ffcd750865237801b4185123b
cyfral123 <pj2fgr7vbb@privaterelay.appleid.com>
2025-12-03 21:45:50 +0300

    new util

---
+++ Utils/Anchor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace HisTools.Utils;
+
+public static class Anchor
+{
+    public static void SetAnchor(RectTransform rt, int pos)
+    {
+        Vector2 pivot;
+
+        var anchor = pos switch
+        {
+            1 => // Top-Left
+                pivot = new Vector2(0, 1),
+            2 => // Top-Center
+                pivot = new Vector2(0.5f, 1),
+            3 => // Top-Right
+                pivot = new Vector2(1, 1),
+            4 => // Bottom-Left
+                pivot = new Vector2(0, 0),
+            5 => // Bottom-Center
+                pivot = new Vector2(0.5f, 0),
+            6 => // Bottom-Right
+                pivot = new Vector2(1, 0),
+            _ => pivot = new Vector2(0.5f, 0.5f)
+        };
+
+        rt.anchorMin = rt.anchorMax = anchor;
+        rt.pivot = pivot;
+        rt.anchoredPosition = Vector2.zero;
+    }
+}
